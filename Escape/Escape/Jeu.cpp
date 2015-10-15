100d399ae15d2bcfe39950364885263ce191da8a
Anthony-Dubois <dubois.anthony.is@gmail.com>
2015-10-15 15:15:46 +0200

    Affichage du personnage

--- Escape/Escape/Jeu.cpp
+++ Escape/Escape/Jeu.cpp
@@ -1,16 +1,35 @@
+#pragma region includes
+//includes sfml
 #include <SFML/Graphics.hpp>
-
-#include <SFML/Graphics.hpp>
+//includes c++
 #include <windows.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#pragma endregion includes
+
+#pragma region variables
+//variables d'écran
+int xScreen = GetSystemMetrics(SM_CXFULLSCREEN);
+int yScreen = GetSystemMetrics(SM_CYFULLSCREEN);
+
+//variables de texture
+sf::Texture hero;
+sf::Sprite sprite_hero;
+#pragma endregion variables
 
 int main(){
-	
-	
-	int xScreen = GetSystemMetrics(SM_CXFULLSCREEN);
-	int yScreen = GetSystemMetrics(SM_CYFULLSCREEN);
+
+	//ouverture de la fenetre
 	sf::RenderWindow window(sf::VideoMode(xScreen, yScreen-10), "Escape", sf::Style::Default);
 	window.setPosition(sf::Vector2i(-10, 0));
+
+	//affichage des sprites
+	if (!hero.loadFromFile("hero.png", sf::IntRect(0,0,32,32))) {
+		std::cout << "Erreur de chargement des sprites" << std::endl;
+	}
+	hero.setSmooth(true);
+	sprite_hero.setTexture(hero);
 
 	while (window.isOpen())
 	{
@@ -21,8 +40,10 @@ int main(){
 				window.close();
 		}
 
-		window.clear();
+		window.draw(sprite_hero);
 		window.display();
+		window.clear();
+		
 	}
 
 	return 0;
